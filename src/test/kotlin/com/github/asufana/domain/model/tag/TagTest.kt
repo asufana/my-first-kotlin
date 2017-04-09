0252fa48b43a96a71f89b47dfd60cba12a24a7b7
hana <asufanah@gmail.com>
2017-04-09 20:12:18 +0900

    refs #4 PostとTagの関連付け

--- src/test/kotlin/com/github/asufana/domain/model/tag/TagTest.kt
+++ src/test/kotlin/com/github/asufana/domain/model/tag/TagTest.kt
@@ -2,6 +2,8 @@ package com.github.asufana.domain.model.tag
 
 import com.github.asufana.domain.T
 import com.github.asufana.domain.base.AbstractTest
+import com.github.asufana.domain.model.post.PostTest
+import com.github.asufana.domain.model.tag.vo.TagName
 import org.assertj.core.api.Assertions.*
 import org.junit.Test
 
@@ -24,5 +26,33 @@ class TagTest : AbstractTest() {
         //保存されること
         assertThat(tag).isNotNull()
         assertThat(tag.isSaved()).isTrue()
+    }
+
+    //Postとの関連付けテスト
+    @Test
+    fun testRelatedPost() {
+        //インスタンス保存
+        val tag01 = Tag(TagName("fun")).save()
+        val tag02 = Tag(TagName("etc")).save()
+        val post = PostTest.save()
+
+        //Postと関連付けられていないこと
+        assertThat(post.tags().count()).isEqualTo(0)
+
+        //---------------------------------------
+
+        //Postとの関連付け
+        val savedPost = post.assign(tag01).assign(tag02)
+
+        //Postと関連付けられていること
+        assertThat(savedPost.tags().count()).isEqualTo(2)
+
+        //---------------------------------------
+
+        //Postとの関連付け解除
+        savedPost.unAssign(tag01).save()
+
+        //Postと関連付けが解除されていること
+        assertThat(savedPost.tags().count()).isEqualTo(1)
     }
 }
